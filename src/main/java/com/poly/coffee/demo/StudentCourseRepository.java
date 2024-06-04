05410c999a80211947db75d383cbc92e8457754c
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-06-04 10:31:40 +0700

    Delete: User and Demo Package

--- src/main/java/com/poly/coffee/demo/StudentCourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.poly.coffee.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentCourseRepository extends JpaRepository<StudentCourse, StudentCourseKey> {
-}
--- src/main/java/com/poly/coffee/service/impl/DrinkSizeServiceImpl.java
+++ src/main/java/com/poly/coffee/service/impl/DrinkSizeServiceImpl.java
@@ -4,16 +4,20 @@ import com.poly.coffee.entity.Drink;
 import com.poly.coffee.entity.DrinkSize;
 import com.poly.coffee.repository.DrinkSizeRepository;
 import com.poly.coffee.service.DrinkSizeService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Service
 public class DrinkSizeServiceImpl implements DrinkSizeService {
 
-    @Autowired
     DrinkSizeRepository drinkSizeRepository;
 
     @Override
