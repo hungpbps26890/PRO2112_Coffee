a3b6d4c1b84f4204acabd3d2c207cd867732b780
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-06-04 11:17:38 +0700

    Add User CRUD Rest API

---
+++ src/main/java/com/poly/coffee/dto/request/UserUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.poly.coffee.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserUpdateRequest {
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dob;
+}
---
+++ src/main/java/com/poly/coffee/entity/User.java
@@ -0,0 +1,24 @@
+package com.poly.coffee.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+}
