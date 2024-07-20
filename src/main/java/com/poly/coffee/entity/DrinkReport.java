0b0599cae86879951f74f174ff89826d390d1c2d
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-07-20 13:29:13 +0700

    Add Report RESTful API

---
+++ src/main/java/com/poly/coffee/entity/DrinkReport.java
@@ -0,0 +1,29 @@
+package com.poly.coffee.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DrinkReport implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Transient
+    private Drink drink;
+    private Double revenue;
+    private Long quantity;
+
+    public DrinkReport(Drink drink, Double revenue, Long quantity) {
+        this.drink = drink;
+        this.revenue = revenue;
+        this.quantity = quantity;
+    }
+}
